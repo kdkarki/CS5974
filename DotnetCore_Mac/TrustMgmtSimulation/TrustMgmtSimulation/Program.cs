cfe163f0424727a8e22a4f82ed994793ad068c17
Kshitij Karki <kdkarki@outlook.com>
2018-01-10 06:45:15 -0500

    Initial commit to dotnet in mac

---
+++ DotnetCore_Mac/TrustMgmtSimulation/TrustMgmtSimulation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TrustMgmtSimulation
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            SMPLWrapper.smpl(0, "Test");
+
+            Console.WriteLine(SMPLWrapper.time());
+        }
+    }
+}
---
+++ DotnetCore_Mac/TrustMgmtSimulation/TrustMgmtSimulation/SMPLWrapper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TrustMgmtSimulation
+{
+    public class SMPLWrapper
+    {
+        public SMPLWrapper()
+        {
+            
+        }
+
+        [DllImport("./libsmpl.dylib", EntryPoint = "smpl")]
+        public static extern void smpl(int id, string moduleName);
+
+        [DllImport("./libsmpl.dylib", EntryPoint = "time")]
+        public static extern double time();
+
+        /*
+         * Following function needs to be imported
+         * 
+            extern real Lq(), U(), B(), time();
+            extern char* fname(), *mname();
+            extern FILE* sendto();
+
+            extern real uniform(), expntl(), erlang(), hyperx(), normal();
+            extern long seed();
+
+            void smpl(int, char*);
+            int facility(char*, int);
+            void schedule(int, real, int);
+            void cause(int*, int*);
+            int request(int, int, int);
+            void release(int, int);
+            void report();
+            void error(int, char*);
+
+            void init_bm(int, int);
+            int obs(real);
+            void civals(real*, real*, int*);
+        *
+        */
+    }
+}
